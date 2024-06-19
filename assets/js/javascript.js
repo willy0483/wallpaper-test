f17a3defb5048ae3c419a11c2f17b361024089b0
willy0483 <willyjensen251@gmail.com>
2024-06-19 20:08:39 +0200

    y

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -18,19 +18,19 @@ img.forEach((e) => {
 });
 
 // hover over logo
-logogrid1.addEventListener("mouseover", (e) => {
+logogrid1.addEventListener("mouseover", () => {
   image2.style.transform = "scale(1.3)";
 });
 
-logogrid1.addEventListener("mouseout", (e) => {
+logogrid1.addEventListener("mouseout", () => {
   image2.style.transform = "scale(1.2)";
 });
 
-logogrid2.addEventListener("mouseover", (e) => {
+logogrid2.addEventListener("mouseover", () => {
   image3.style.transform = "scale(1.3)";
 });
 
-logogrid2.addEventListener("mouseout", (e) => {
+logogrid2.addEventListener("mouseout", () => {
   image3.style.transform = "scale(1.2)";
 });
 
@@ -39,11 +39,19 @@ const legion = document.getElementById("legion");
 const legionImage = document.getElementById("legionImage");
 
 image2.addEventListener("click", (e) => {
+  // Scroll to the top of the page
+  window.scrollTo({ top: 0, behavior: "smooth" });
+
+  // Show the dragon element and hide the container element
   legion.style.display = "block";
   container.style.display = "none";
 });
 
 legionImage.addEventListener("click", (e) => {
+  // Scroll to the top of the page
+  window.scrollTo({ top: 0, behavior: "smooth" });
+
+  // Show the dragon element and hide the container element
   legion.style.display = "none";
   container.style.display = "block";
 });
@@ -53,11 +61,19 @@ const king = document.getElementById("king");
 const kingImage = document.getElementById("kingImage");
 
 image4.addEventListener("click", (e) => {
+  // Scroll to the top of the page
+  window.scrollTo({ top: 0, behavior: "smooth" });
+
+  // Show the dragon element and hide the container element
   king.style.display = "block";
   container.style.display = "none";
 });
 
 kingImage.addEventListener("click", (e) => {
+  // Scroll to the top of the page
+  window.scrollTo({ top: 0, behavior: "smooth" });
+
+  // Show the dragon element and hide the container element
   king.style.display = "none";
   container.style.display = "block";
 });
@@ -67,11 +83,19 @@ const panda = document.getElementById("panda");
 const pandaImage = document.getElementById("pandaImage");
 
 image3.addEventListener("click", (e) => {
+  // Scroll to the top of the page
+  window.scrollTo({ top: 0, behavior: "smooth" });
+
+  // Show the dragon element and hide the container element
   panda.style.display = "block";
   container.style.display = "none";
 });
 
 pandaImage.addEventListener("click", (e) => {
+  // Scroll to the top of the page
+  window.scrollTo({ top: 0, behavior: "smooth" });
+
+  // Show the dragon element and hide the container element
   panda.style.display = "none";
   container.style.display = "block";
 });
@@ -81,11 +105,33 @@ const dragon = document.getElementById("dragon");
 const dragonImage = document.getElementById("dragonImage");
 
 image1.addEventListener("click", (e) => {
+  // Scroll to the top of the page
+  window.scrollTo({ top: 0, behavior: "smooth" });
+
+  // Show the dragon element and hide the container element
   dragon.style.display = "block";
   container.style.display = "none";
 });
 
 dragonImage.addEventListener("click", (e) => {
+  // Scroll to the top of the page
+  window.scrollTo({ top: 0, behavior: "smooth" });
+
+  // Show the dragon element and hide the container element
   dragon.style.display = "none";
   container.style.display = "block";
+});
+
+const srcollDown = document.getElementById("srcollDown");
+
+srcollDown.addEventListener("click", (e) => {
+  e.preventDefault();
+  const currentScrollPosition =
+    window.scrollY || document.documentElement.scrollTop;
+
+  if (currentScrollPosition === 0) {
+    window.scrollTo({ top: 1000, behavior: "smooth" });
+  } else {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
 });
